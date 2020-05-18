63d98539a6ea05146225d55de550fd2504944dd7
mbake94 <mbake94@gmail.com>
2020-05-18 11:16:01 -0700

    first commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+public class Student {
+    private String name, phone, height;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
